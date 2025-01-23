3e9e3e8df17d4ec382a58d22b2e55f2b8d7015d3
Malzagic <przemek2142@gmail.com>
2025-01-23 18:37:23 +0100

    add new step 2 from the part1.7 and additional css

--- part1/part1.6-1.11 unicafe/src/App.jsx
+++ part1/part1.6-1.11 unicafe/src/App.jsx
@@ -4,13 +4,19 @@ const Header = ({ text }) => {
   return <h1>{text}</h1>;
 };
 
-const Statistic = ({ text, good, neutral, bad }) => {
+const Statistic = ({ text, good, neutral, bad, total, avarage, positive }) => {
   return (
     <>
       <h2>{text}</h2>
-      <p>good: {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        {/* Version from part 1.7 Step 2 */}
+        <li>all {total}</li>
+        <li>avarage {avarage}</li>
+        <li>positive {positive} %</li>
+      </ul>
     </>
   );
 };
@@ -25,6 +31,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  //* Version from part 1.7 Step 2
+  const total = good + neutral + bad;
+  const average = total > 0 ? (good - bad) / total : 0;
+  const positivePercentage = total > 0 ? (good / total) * 100 : 0;
+
   return (
     <div>
       {/* Version from part 1.6 setp 1 */}
@@ -32,7 +43,16 @@ const App = () => {
       <Button text="good" onClick={() => setGood(good + 1)} />
       <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" onClick={() => setBad(bad + 1)} />
-      <Statistic text="Statistics" good={good} neutral={neutral} bad={bad} />
+      <Statistic
+        text="Statistics"
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        // Version from part 1.7 Step 2
+        total={total}
+        avarage={average}
+        positive={positivePercentage}
+      />
     </div>
   );
 };
