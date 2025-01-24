6a462e7cf1cf9607ae3360ac0002c6fec9fe42b6
Malzagic <przemek2142@gmail.com>
2025-01-24 14:50:33 +0100

    completed task 2.6

---
+++ Part2/part2.6/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const inputNameHandler = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addNewNameHandler = (e) => {
+    e.preventDefault();
+
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewNameHandler}>
+        <div>
+          name: <input onChange={inputNameHandler} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{ listStyle: "none", paddingLeft: 0 }}>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
